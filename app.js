6c186b13af08723560d0b4eb38b846d4e1264f69
Tanuj Kumar <tk923323@gmail.com>
2022-04-29 12:04:43 +0530

    edit main file

--- app.js
+++ app.js
@@ -4,32 +4,13 @@ const cors = require("cors");
 const app = express();
 require('dotenv').config()
 
-app.use(cors({
-    origin:'*'
-}));
 
-const con = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password: `${process.env.PASSWORD}#6842`,
-    insecureAuth : true,
-    database:'reactn'
-});
+
 app.get('/',(req,res)=>{
     console.log("Hello from Project")         
     res.send("Hello from Project")
 })
-app.get('/gettingdata',(req,res)=>{
-    con.connect(function(err) {
-    if (err) throw err;
-        con.query("SELECT * FROM userdata", function (err, result) {
-        if (err) throw err;
-        else{
-            res.send(result)
-        }    
-        });
-    });
-})
+
 
 const port=process.env.PORT || 8001
 app.listen(port, () => {
